7cfc63693822b3b1f5588846834600b1814c4471
Akash Varshney <akash.varshney@gmail.com>
2022-02-04 18:55:54 +0530

    Add project files.

---
+++ WeatherInfo.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebUI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+    public class Client
+    {   
+       
+
+        static HttpClient client = new HttpClient();
+
+        public static async Task<List<WeatherForecast>> GetData(string uri)
+        {           
+            
+            HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<WeatherForecast>>(responseBody);
+            return result;
+        }
+
+    }
+
+
+
+}
