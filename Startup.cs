3235a8b8c6f9390b3750f64aa6b93874998c5cc0
Akash Varshney <akash.varshney@gmail.com>
2022-02-07 10:32:01 +0530

    update route to webUI from default

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -45,6 +46,17 @@ namespace WebUI
             app.UseStaticFiles();
 
             app.UseRouting();
+         
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/")
+                {
+                    context.Response.Redirect("WebUI/", true);
+
+                    return;
+                }
+                await next();
+            });
 
             app.UseAuthorization();
 
